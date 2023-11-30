59f90c2670d121214a33f81cfd5a7b7f7c3e1154
Kabina Thapa <thapakabina28@gmail.com>
2023-11-30 16:18:08 -0500

    fix: correct sticky behaviour in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,87 @@ function App() {
 
   return (
     <>
-    <div>
-    <h1>Kabina Thapa</h1>
+    <div className='container flex p-2 justify-between'>
+      <div className='sticky top-0 border w-[40%] h-96'>
+      <h1>Kabina Thapa</h1>
+      <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+      
+      </div>
+   <div className='second w-[40%]'>
+    <h1>experiences</h1>
+    <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+         <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+         <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+         <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+         <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+         <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+         <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+         <div className='md:text-lg text-sm'>
+        <p>As a passionate and self-motivated aspiring front-end developer, 
+        I am dedicated to continuously expanding my knowledge and skills in web development. 
+        Eager to embark on a career in front-end development, I have been actively learning 
+        and working on personal projects to build a strong foundation in HTML, CSS, JavaScript,
+         and front-end frameworks like React. My drive to excel and adapt to emerging technologies 
+         makes me a determined and promising candidate eager to contribute to a dynamic team.</p>
+         </div>
+    
+   </div>
     </div>
     
      
